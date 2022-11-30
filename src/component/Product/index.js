485100f330fcb8947eb29c5f184b258c039e1347
Matarov <moe.matar1998@gmail.com>
2022-12-01 01:29:42 +0200

    fetched products data

--- src/component/Product/index.js
+++ src/component/Product/index.js
@@ -14,7 +14,7 @@ export default function Product() {
 
     const getAll = () => {
         axios
-            .get("http://localhost:8080/categories")
+            .get("http://localhost:8080/items")
             .then((res) => {
                 setProduct(res.data.data);
                 console.log(res.data.data)
@@ -25,46 +25,46 @@ export default function Product() {
             });
     };
     return (<>
-        <div className='product__container'>
-            {isLoading ? (
-                <div class="lds-roller">
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                </div>
-            ) : (
-                product.map((data) => {
-                    return (
-                        <>
+        {isLoading ? (
+            <div class="lds-roller">
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        ) : (
+            product.map((data) => {
+                return (
+                    <>
+                        <div className='product__container'>
+
                             <div className='product-half__container'>
                                 <div className='product-image'>
                                     <img src={backgroundimg} />
                                 </div>
                                 <div className='product-description'>
                                     <p>
-                                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                                        {data.description}
                                     </p>
                                 </div>
                                 <div className='product_title'>
                                     <h1>
-                                        Title
+                                        {data.title}
                                     </h1>
-                                    <h1> Price: $</h1>
+                                    <h1> Price: ${data.price}</h1>
                                 </div>
                             </div>
-                        </>
-                    )
-                })
-            )
-            }
+                        </div>
 
-        </div>
-
+                    </>
+                )
+            })
+        )
+        }
     </>
     )
 }   
