25aed0c816ef7874b276fdead37ebeb3c83c4017
Matarov <moe.matar1998@gmail.com>
2022-12-01 01:23:47 +0200

    fetching the category data

--- src/component/Category/index.js
+++ src/component/Category/index.js
@@ -1,20 +1,62 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
 import backgroundimg from "../../images/backgroundimg.jpg"
 
 import "./style.css"
 
 export default function Category() {
+    const [data, setData] = useState();
+    const [isloading, setIsloading] = useState(true)
+
+    useEffect(() => {
+        getAll();
+    }, []);
+    const getAll = () => {
+        axios
+            .get("http://localhost:8080/categories")
+            .then((res) => {
+                setData(res.data.data);
+                console.log(res.data.data)
+                setIsloading(false);
+            })
+            .catch((err) => {
+                console.error(err);
+            });
+    };
+
     return (
         <div className='category_container'>
-            <div className='category-img-card'>
-                <img src={backgroundimg} className='category_img' />
-            </div>
+            {isloading ? (
+                <div class="lds-roller">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+            ) : (
+                data.map((data) => {
+                    return (
+                        <>
+                            <div className='category-img-card'>
+                                <img src={`http://localhost:8080/categories/upload/${data.icon}`} className='category_img' />
+                            </div>
 
-            <div className='category_name'>
-                <h1>
-                    Name
-                </h1>
-            </div>
+                            <div className='category_name'>
+                                <h1>
+                                    {data.title}
+                                </h1>
+                            </div>
+                        </>
+                    )
+                })
+
+            )
+            }
+
         </div>
 
     )
--- src/component/Product/index.js
+++ src/component/Product/index.js
@@ -1,67 +1,70 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
 import backgroundimg from "../../images/backgroundimg.jpg"
 
 import "./style.css"
 
 export default function Product() {
-    
+    const [product, setProduct] = useState();
+    const [isLoading, setIsLoading] = useState(true)
 
-    
+    useEffect(() => {
+        getAll();
+    }, []);
+
+    const getAll = () => {
+        axios
+            .get("http://localhost:8080/categories")
+            .then((res) => {
+                setProduct(res.data.data);
+                console.log(res.data.data)
+                setIsLoading(false);
+            })
+            .catch((err) => {
+                console.error(err);
+            });
+    };
     return (<>
         <div className='product__container'>
-            <div className='product-half__container'>
-                <div className='product-image'>
-                    <img src={backgroundimg} />
+            {isLoading ? (
+                <div class="lds-roller">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
                 </div>
-                <div className='product-description'>
-                    <p>
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-                    </p>
-                </div>
-                <div className='product_title'>
-                    <h1>
-                        Title
-                    </h1>
-                    <h1> Price: $</h1>
-                </div>
-            </div>
+            ) : (
+                product.map((data) => {
+                    return (
+                        <>
+                            <div className='product-half__container'>
+                                <div className='product-image'>
+                                    <img src={backgroundimg} />
+                                </div>
+                                <div className='product-description'>
+                                    <p>
+                                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                                    </p>
+                                </div>
+                                <div className='product_title'>
+                                    <h1>
+                                        Title
+                                    </h1>
+                                    <h1> Price: $</h1>
+                                </div>
+                            </div>
+                        </>
+                    )
+                })
+            )
+            }
+
         </div>
-        <div className='product__container'>
-            <div className='product-half__container'>
-                <div className='product-image'>
-                    <img src={backgroundimg} />
-                </div>
-                <div className='product-description'>
-                    <p>
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-                    </p>
-                </div>
-                <div className='product_title'>
-                    <h1>
-                        Title
-                    </h1>
-                    <h1> Price: $</h1>
-                </div>
-            </div>
-        </div>
-        <div className='product__container'>
-            <div className='product-half__container'>
-                <div className='product-image'>
-                    <img src={backgroundimg} />
-                </div>
-                <div className='product-description'>
-                    <p>
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-                    </p>
-                </div>
-                <div className='product_title'>
-                    <h1>
-                        Title
-                    </h1>
-                    <h1> Price: $</h1>
-                </div>
-            </div>
-        </div>
+
     </>
     )
 }   
